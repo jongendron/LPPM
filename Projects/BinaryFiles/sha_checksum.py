8734e6f6afdbb2b62aa839c92f8c9123e64ce5d7
jongendron <jonmgendron@gmail.com>
2023-02-20 14:06:54 -0800

    Update Colorama 1

--- Projects/BinaryFiles/sha_checksum.py
+++ Projects/BinaryFiles/sha_checksum.py
@@ -1,5 +1,11 @@
 import hashlib
 
-published_hash = '08695f5cb7ed6e0531a20572697297273c47b8cae5a63ffc6d6ed5c201be6e44'
+published_hash = '4f1d9991f5acc0ca119f9d443620b77f9d6b33703e51011c16baf57afb285fc6'
 
-filename = 'colorama-0.4'
+filename = 'colorama-0.4.6-py2.py3-none-any.whl'
+
+with open(filename, 'rb') as downloaded_file:
+    contents = downloaded_file.read()
+
+file_hash = hashlib.sha256(contents).hexdigest()
+print(file_hash)
