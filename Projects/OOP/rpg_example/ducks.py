db6387b5d1339a13f5212c072062efeb09b0afb1
jongendron <jonathan.gendron@wsu.edu>
2023-04-06 13:28:54 -0700

    Composition: duck is composed of a wing

--- Projects/OOP/rpg_example/ducks.py
+++ Projects/OOP/rpg_example/ducks.py
@@ -6,7 +6,30 @@
 # Example: when calling the print(<object>) function, any object can be passed as long as it has a __str__ method (which all do by default because of the object metaclass)
 # -> This is therefor polymorphism!
 
+# Composition is when you build a parent class using other classes to define its attributes
+# For example Ducks are composed of Wings in the example below.
+
+class Wing(object):
+    """Wing Class"""
+    
+    def __init__(self, ratio):
+        self.ratio = ratio
+
+    def fly(self):
+        """Fly method"""
+        if self.ratio > 1:
+            print("Weee, this is fun!")
+        elif self.ratio == 1:
+            print("This is hard work, but I'm flying.")
+        else:
+            print("I think I'll just walk ...")
+
+
 class Duck(object):
+    """Duck class"""
+
+    def __init__(self):
+        self._wing = Wing(1.8) # Creates attribute that is an instance of the class Wing.
 
     def walk(self):
         print("Waddle, waddle, waddle.")
@@ -17,8 +40,11 @@ class Duck(object):
     def quack(self):
         print("Quack, quack!")
 
+    def fly(self):
+        self._wing.fly() # calls on a method in the Wing class instance (_wing).
 
 class Penguin(object):
+    """Penguin Class"""
 
     def walk(self):
         print("Waddle, waddle, I waddle too.")
@@ -30,14 +56,12 @@ class Penguin(object):
         print("Are you having a laugh? I'm a penguin!")
 
 
-def test_duck(duck):
-    duck.walk()
-    duck.swim()
-    duck.quack()
+# def test_duck(duck):
+#     duck.walk()
+#     duck.swim()
+#     duck.quack()
 
 if __name__ == '__main__':
     donald = Duck()
-    test_duck(donald)
-
-    percy = Penguin() # percy is a duck according to the test_duck method (because Python only care's about behavior rather than class)
-    test_duck(percy) # Penguin and Duck classes both have walk, swim, and quack methods ... therefore test_duck assumes it passes as a duck!
+    donald.fly() # calls on donald._wing attribute's method fly() (donald._wing.fly())
+ 
