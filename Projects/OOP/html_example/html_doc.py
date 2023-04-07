e3bf2f673c2e541a277fa904374608bf7dbf5db3
jongendron <jonathan.gendron@wsu.edu>
2023-04-07 14:09:17 -0700

    Aggregation

--- Projects/OOP/html_example/html_doc.py
+++ Projects/OOP/html_example/html_doc.py
@@ -56,10 +56,15 @@ class Body(Tag):
 # HtmlDoc class is composed of DocType, Head, and Body classes (Composition)
 class HtmlDoc(object):
 
-    def __init__(self, title=None):
-        self._doc_type = DocType()
-        self._head = Head(title=title)
-        self._body = Body()
+    # def __init__(self, title=None): # composition because attributes are class instances created within not saved as
+    #     self._doc_type = DocType()
+    #     self._head = Head(title=title)
+    #     self._body = Body()
+
+    def __init__(self, doc_type, head, body): # aggregation because instances are not created within the composition class, rather just saved as such
+        self._doc_type = doc_type
+        self._head = head
+        self._body = body
 
     def add_tag(self, name, contents): # purposly had same same so the same method is always done whereever .add_tag is seen
         self._body.add_tag(name, contents)
@@ -72,19 +77,29 @@ class HtmlDoc(object):
         print('</html>', file=file)
 
 if __name__ == '__main__':
-    my_page = HtmlDoc(title='Document Title')
-    my_page.add_tag('h1', 'Main Heading')
-    my_page.add_tag('h2', 'sub-heading')
-    my_page.add_tag('p', 'This is a paragraph that will appear on the page.')
-    
-    with open("test.html", "w") as test_doc:
-        my_page.display(file=test_doc)
+    # my_page = HtmlDoc(title='Document Title')
+    # my_page.add_tag('h1', 'Main Heading')
+    # my_page.add_tag('h2', 'sub-heading')
+    # my_page.add_tag('p', 'This is a paragraph that will appear on the page.')
+    # with open("test.html", "w") as test_doc:
+    #     my_page.display(file=test_doc)
 
-# TODO: Modify program so that Head class can include a Title tag.
-# <doctype info>
-# <html>
-# <head>
-#   <title> stuff </title>
-# </head>
-# <body> stuff </body>
-# </html>
+    # Aggregation (has-a relation, but the superclass doesn't own the objects like in composition)
+    # If the superclass instance is deleted, the classes it's composed of are not deleted in aggregation
+    # Aggregation is convienient when you want class instances independent of a container class object (instance)
+    new_body = Body()
+    new_body.add_tag('h1', 'Aggregation')
+    new_body.add_tag('p', "Unlike <strong>composition</strong>, aggregation uses existing instances"
+                     " of objects to build up another object.")
+    new_body.add_tag('p', "The composed object doesn't actually own the objects that it's composed of "
+                     " - if it's destroyed, those objects continue to exist.")
+    
+    new_docType = DocType()
+    new_header = Head('Aggregation document')
+
+    my_page = HtmlDoc(new_docType, new_header, new_body)
+
+    # Give our document new content by switching it's body
+    my_page._body = new_body
+    with open('test2.html', 'w') as test_doc:
+        my_page.display(file=test_doc)
