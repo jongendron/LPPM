cb84f94569045ec6f1dc8dd9c2dca51d25cb1c07
jongendron <jonathan.gendron@wsu.edu>
2023-04-12 15:39:48 -0700

    SQLite Substitution

--- Projects/Databases/CreateDB/contacts2.py
+++ Projects/Databases/CreateDB/contacts2.py
@@ -4,12 +4,23 @@ db = sqlite3.connect("contacts.db") # the connection contains a cursor when crea
 
 # Command to count rows updated by SQLite command
 # In this case we are executing to the cursor not the connection directly
+# Using variable substituion now (Python method and SQLite method)
 
-#update_sql = "UPDATE  contacts SET email = 'update@update.com' WHERE contacts.phone = 4567" # saved command
-update_sql = "UPDATE  contacts SET email = 'update@update.com'" # saved command
+new_email = "newest-update@update.com"
+#phone = 1234
+phone = input("Please enter the phone number: ")
+
+update_sql = "UPDATE contacts SET email = ? WHERE contacts.phone = ?" # SQLite substitution (don't enclose ? in single or double quote)
+# update_sql = "UPDATE contacts SET email = '{}' WHERE contacts.phone = {}".format(new_email, phone) # Python substitution
+#update_sql = "UPDATE contacts SET email = 'update@update.com' WHERE contacts.phone = 1234" # saved command
+#update_sql = "UPDATE contacts SET email = 'update@update.com'" # saved command
+print(update_sql)
 
 update_cursor = db.cursor() # creates new cursor with same position as db connection (essentially resets it)
-update_cursor.execute(update_sql)
+#update_cursor.execute(update_sql) # designed to execute single sql statement.
+#update_cursor.executescript(update_sql) # designed to run more than 1 sql statement in single call, separated by ";"s. Doesn't set the line counter correctly though b/c of this.
+update_cursor.execute(update_sql, (new_email, phone)) # SQLite variable substitution (placeholders). This is safer against INJECTION ATTACKS. Avoid external parameter substitution.
+
 print("{} rows updated".format(update_cursor.rowcount))
 print()
 print("Cursor and DB object have same connection: {}".format(update_cursor.connection == db))
