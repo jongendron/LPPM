a2fb92008e3d24b0091841b6a5e6dc7e7f2089c1
jongendron <jonathan.gendron@wsu.edu>
2023-04-12 16:08:12 -0700

    SQLite Substitution Challenge

--- Projects/Databases/CreateDB/checkdb.py
+++ Projects/Databases/CreateDB/checkdb.py
@@ -1,12 +1,19 @@
+# TODO: Modify this program so it asks user to enter a name, then use a WHERE clause in the SQL statement to retrieve just the row for that one person.
+
 import sqlite3
 
 db = sqlite3.connect("contacts.db")
 
+sqlcmd = "SELECT * FROM contacts WHERE contacts.name LIKE ?"
+name = input("Specify a name: ")
+
 print('-' * 20)
-for name, phone, email in db.execute("SELECT * FROM contacts"):
+#for name, phone, email in db.execute("SELECT * FROM contacts"):
+for name, phone, email in db.execute(sqlcmd, (name,)): # tuples with one element need comma, otherwise its saved as a string, and this will pass as multiple arguements, equal to length string
     print(name)
     print(phone)
     print(email)
     print('-' * 20)
 
 db.close()
+
